58e08ee1cd99d5bf5680ef2fef875e75b226188e
luxm123 <xuanmeilu158@gmail.com>
2026-04-29 20:35:00 +0800

    Optimize deploy.sh with wait command to prevent ResourceConflictException

--- functions/deploy.sh
+++ functions/deploy.sh
@@ -26,8 +26,10 @@ for i in "${!FUNCTIONS[@]}"; do
     
     if [ $? -eq 0 ]; then
         echo "Updating $FUNC_NAME..."
-        aws lambda update-function-code --function-name $FUNC_NAME --zip-file fileb://$FUNC_NAME.zip --region $REGION
-        aws lambda update-function-configuration --function-name $FUNC_NAME --memory-size $MEMORY --region $REGION
+        aws lambda update-function-code --function-name $FUNC_NAME --zip-file fileb://$FUNC_NAME.zip --region $REGION > /dev/null
+        echo "Waiting for $FUNC_NAME to finish updating..."
+        aws lambda wait function-updated --function-name $FUNC_NAME --region $REGION
+        aws lambda update-function-configuration --function-name $FUNC_NAME --memory-size $MEMORY --region $REGION > /dev/null
     else
         echo "Creating $FUNC_NAME..."
         aws lambda create-function --function-name $FUNC_NAME \
@@ -36,7 +38,9 @@ for i in "${!FUNCTIONS[@]}"; do
             --handler lambda_handler.lambda_handler \
             --zip-file fileb://$FUNC_NAME.zip \
             --memory-size $MEMORY \
-            --region $REGION
+            --region $REGION > /dev/null
+        echo "Waiting for $FUNC_NAME to be active..."
+        aws lambda wait function-active --function-name $FUNC_NAME --region $REGION
     fi
     
     rm build/lambda_handler.py
